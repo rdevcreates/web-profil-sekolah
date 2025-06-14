25d96b68ed545b8ef1aacffd63233ed4af4b0c3e
pddccvv <rudiyat404@gmail.com>
2025-06-14 21:08:24 +0700

    add section extracurricular

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 import SectionWelcome from "./components/section/home/SectionWelcome";
 import SectionHistory from "./components/section/home/SectionHistory";
 import SectionArticle from "./components/section/home/SectionArticle";
+import SectionExtracurricular from "./components/section/home/SectionExtracurricular";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <SectionWelcome/>
       <SectionHistory/>
+      <SectionExtracurricular/>
       <SectionArticle/>
     </div>
   );
